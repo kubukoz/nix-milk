d70d8fece55519092922bd21c8b77c52c5dce03c
Jakub Kozłowski <kubukoz@gmail.com>
2021-11-25 22:46:35 +0100

    Separate sha for linux

--- derivation.nix
+++ derivation.nix
@@ -1,4 +1,4 @@
-{ jre, sbt, gitignore-source, makeWrapper }:
+{ jre, sbt, gitignore-source, makeWrapper, stdenv }:
 
 let
   mainClass = "com.kubukoz.nixmilk.Hello";
@@ -6,7 +6,10 @@ in
 sbt.mkDerivation rec {
   pname = "sbt-nix-nix-milk";
   version = "0.1.0";
-  depsSha256 = "sha256-qICtWWkxarGlnPjaEP1tp5dhYckfyOHA+V5Cy9JbLRI=";
+  depsSha256 =
+    if stdenv.isDarwin
+    then "sha256-qICtWWkxarGlnPjaEP1tp5dhYckfyOHA+V5Cy9JbLRI="
+    else "sha256-i3b/dRrienPTm4KTz9/f16RbgmgfqUlQIIQ6SNytj1Q=";
 
   depsWarmupCommand = ''
     sbt compile
